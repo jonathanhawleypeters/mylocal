45ff600cedefe3ca7085395207fefa6c8a57b7af
Jonathan Hawley-Peters <jhawleypeters@gmail.com>
2016-09-24 18:06:21 -0700

    Add Location and LocationSearch components

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import NavBar from './Navbar';
-import Hero from './Hero'; 
+import Hero from './Hero';
+import Location from './Location';
 import Categories from './Categories';
 import HowItWorks from './HowItWorks'; 
 import Footer from './Footer'; 
 
 export default class App extends Component {
   render() {
+    //Replaced Hero with Location for testing
     return (
       <div>
         <NavBar />
         <Hero />
+        <Location />
         <Categories />
         <HowItWorks />
         <Footer />
