ef8cab38ed0c0e22d58bb9d5135a8c717075c76b
Yasser Mahmud <yasser.mahmud@gmail.com>
2016-09-28 21:07:45 -0700

    Logo added

--- src/client/components/NavBar.js
+++ src/client/components/NavBar.js
@@ -7,10 +7,10 @@ import { connect } from 'react-redux';
   isAuthenticated() {
     if (!this.props.authenticated) {
       return [
-        <li className="nav-item" key={1}>
+        <li className="nav-item" key={ 1 }>
           <Link to="/signin" className="nav-item nav-link active">Sign in</Link>
         </li>,
-        <li className="nav-item" key={2}>
+        <li className="nav-item" key={ 2 }>
           <Link to="/signup" className="nav-item nav-link active">Sign up</Link>
         </li>
       ]
@@ -26,25 +26,25 @@ import { connect } from 'react-redux';
   render() {
     return (
       <header className="navbar navbar-light navbar-fixed-top bd-navbar">
-        <div className="container-fluid">
-          <nav>
+        <div className="container-fluid navbar-fluid">
+          <nav style={{ 'font-size': '20px' }}>
             <div className="clearfix">
-              <button className="navbar-toggler pull-xs-right hidden-sm-up" style={{outline:'none'}} type="button" data-toggle="collapse" data-target="#bd-main-nav" aria-controls="bd-main-nav" aria-expanded="false" aria-label="Toggle navigation">
+              <button className="navbar-toggler pull-xs-right hidden-sm-up" style={{ 'outline':'none', 'margin-top': '13px', 'color': '#999' }} type="button" data-toggle="collapse" data-target="#bd-main-nav" aria-controls="bd-main-nav" aria-expanded="false" aria-label="Toggle navigation">
                 ☰
               </button>
-              <Link className="navbar-brand hidden-sm-up" to="/">My Local</Link>
+              <Link className="navbar-brand hidden-sm-up" to="/"><img src="img/logo.svg" style={{ 'width': '80px' }} /></Link>
             </div>
             <div className="collapse navbar-toggleable-xs" id="bd-main-nav">
               <ul className="nav navbar-nav">
                 <li className="nav-item hidden-xs-down">
-                  <Link className="navbar-brand" to="/">My Local</Link>
+                  <a className="navbar-brand" href="#"><img src="img/logo.svg" style={{ 'width': '80px' }}/></a>
                 </li>
               </ul>
-              <ul className="nav navbar-nav pull-sm-right">
+              <ul className="nav navbar-nav pull-sm-right" style={{ 'margin-top': '13px'}}>
                 <li className="nav-item">
-                  <Link to="/search" className="nav-item nav-link active">Home</Link>
+                  <Link className="nav-item nav-link active" to="/search">Home</Link>
                 </li>
-               {this.isAuthenticated()}
+                {this.isAuthenticated()}
               </ul>
             </div>
           </nav>
