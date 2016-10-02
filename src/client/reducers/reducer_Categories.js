75a7233f449eeb989d55d15ae5cb389322d9536f
Yasser Mahmud <yasser.mahmud@gmail.com>
2016-10-02 12:39:15 -0700

    Categores configured as container and moved

---
+++ src/client/reducers/reducer_Categories.js
@@ -0,0 +1,20 @@
+export default function () {
+  return [
+      {
+        header: "Find Restaurants",
+        image: "img/restaurant-banner.jpg"
+      },
+      {
+        header: "Find Events",
+        image: "img/events-banner.jpg"
+      },
+      {
+        header: "Find Services",
+        image: "img/jobs-banner.jpg"
+      },
+      {
+        header: "Find Volunteers",
+        image: "img/community-banner.jpg",
+      }
+    ];
+}
